d3dc754051e0291ec0cd8a565dddc0a5436c609b
Hyunjulie <sg04088@gmail.com>
2018-11-12 20:14:14 +0800

    Create Basis for project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import _ from 'lodash';
+import './style.css';
+// import Icon from './img.png';
+import printMe from './print.js';
+import message from './message';
+
 
 function component() {
   let element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.classList.add('hello');
 
+  // var myIcon = new Image();
+  // myIcon.src = Icon;
+  // element.appendChild(myIcon);
+
+  element.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
+
+const paragraph = document.createElement('p');
+paragraph.innerHTML = message;
+document.body.prepend(paragraph);
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const env = process.env.NODE_ENV || 'development';
+const isDev = env == 'development';
+const isProd = env == 'production';
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: ['./src/index.js', './src/print.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin(),
+    ],
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+          ]
+      }
+    ]
   }
 };
