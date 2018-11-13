d7b662dee1f554f39fd7ecd970882a759e686bf0
Hyunjulie <sg04088@gmail.com>
2018-11-13 22:34:41 +0800

    Reorganizing materials

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,8 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-const env = process.env.NODE_ENV || 'development';
-const isDev = env == 'development';
-const isProd = env == 'production';
 
 module.exports = {
-  entry: {
-    bundle: ['./src/index.js', './src/print.js']
+  entry: { bundle: ['./src/index.js','./src/print.js']
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin(),
-    ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -37,7 +26,19 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader'
-          ]
+        ]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
       }
     ]
   }
